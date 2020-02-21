70c1fc818e807207bd383c88b208d18a4eee53fb
guanzongyi@eq.com <guanzongyi@eq.com>
2020-02-21 22:55:42 +0800

    react-guan

---
+++ src/config/menuConfig.js
@@ -0,0 +1,14 @@
+const menuList = [{
+        title: '首页',
+        key: '/home'
+    },
+    {
+        title: '用户',
+        key: '/user',
+        children: [{
+            title: '用户设置',
+            key: '/user',
+        }]
+    }
+];
+export default menuList;
