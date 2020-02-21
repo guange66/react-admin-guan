e3af410d2a4af1e539c36c9ea8b28531ef3fc50b
guanzongyi@eq.com <guanzongyi@eq.com>
2020-02-21 22:02:16 +0800

    清空

--- src/pages/home/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default class Home extends React.Component {
-  render() {
-    return <div className="home-wrap"  >欢迎学习IMooc后台管理系统课程 </div>;
-  }
-}
--- src/pages/user/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-export default class User extends React.Component {
-    render() {
-        return <div className = "home-wrap" > 用户页面 </div>;
-    }
-}
